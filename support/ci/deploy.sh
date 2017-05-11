5d63f593ceae5828e6bae432111c5dbaa6a53553
Matt Wrock <matt@mattwrock.com>
2017-05-11 16:07:41 -0700

    unfinished work to fix concurrent mac builds
    Signed-off-by: Matt Wrock <matt@mattwrock.com>

--- support/ci/deploy.sh
+++ support/ci/deploy.sh
@@ -27,16 +27,28 @@ wget -O hab.tar.gz "${HAB_DOWNLOAD_URL}"
 tar xvzf ./hab.tar.gz --strip 1 -C ${BOOTSTRAP_DIR}
 
 # kick off the mac unstable build
-echo "Kicking off the unstable mac build"
+echo "Kicking off the $BINTRAY_REPO mac build"
 var_file=/tmp/our-awesome-vars
 mac_builder=admin@74.80.245.236
+
+#!/bin/bash
+
+set -eu
+
+hab_src_dir="$HOME/code/$TRAVIS_BUILD_NUMBER"
+mkdir -p ${hab_src_dir}
+cd ${hab_src_dir}
+git clone https://github.com/habitat-sh/habitat -b $TRAVIS_COMMIT
+cd habitat
+chmod 755 support/ci/deploy_mac.sh
+
 
 # first update the copy of the habitat code stored on the mac server to the latest
 ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
   -i /tmp/habitat-srv-admin ${mac_builder} \
   "~/bin/update_habitat_code.sh"
 
-set -e
+set -e  
 
 # passing environment variables over ssh is a pain and never worked quite right.
 # instead, write this out to a file and scp it over, to source later.
@@ -46,6 +58,8 @@ export HAB_ORIGIN_KEY=$HAB_ORIGIN_KEY
 export BINTRAY_USER=$BINTRAY_USER
 export BINTRAY_KEY=$BINTRAY_KEY
 export BINTRAY_PASSPHRASE=$BINTRAY_PASSPHRASE
+export TRAVIS_BUILD=$TRAVIS_BUILD_NUMBER
+export GIT_COMMIT=$TRAVIS_COMMIT
 EOF
 
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null \
